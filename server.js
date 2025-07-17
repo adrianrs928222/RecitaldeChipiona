19a116f605d8984798bfabb63f36142304c0f5a4
adrianrs928222 <adrianrs928222@gmail.com>
2025-07-17 19:22:20 +0200

    server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
