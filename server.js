65588a4b2ad17a1d95d73496400b2f599334a1d5
adrianrs928222 <adrianrs928222@gmail.com>
2025-07-17 19:00:22 +0200

    Se agrega el Proyecto

---
+++ server.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import Stripe from 'stripe';
+import cors from 'cors';
+import path from 'path';
+import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+
+dotenv.config();
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+
+app.post('/create-checkout-session', async (req, res) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: [{
+        price_data: {
+          currency: 'eur',
+          product_data: {
+            name: 'Entrada Recital Flamenco 6 de agosto',
+          },
+          unit_amount: 500,
+        },
+        quantity: 1,
+      }],
+      mode: 'payment',
+      success_url: 'https://one-store-95m5.onrender.com/success',
+      cancel_url: 'https://one-store-95m5.onrender.com/cancel',
+    });
+    res.json({ url: session.url });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(PORT, () => console.log(`Servidor funcionando en puerto ${PORT}`));
